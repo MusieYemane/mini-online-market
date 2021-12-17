3e47bb348352e61c77d841c812d36dabdb04a398
emad nasser <ehnasser12@cit.just.edu.jo>
2021-12-17 16:18:47 -0600

    fix buyer

--- src/helper/axiosApiInstance.js
+++ src/helper/axiosApiInstance.js
@@ -5,16 +5,19 @@ const axiosApiInstance = axios.create();
 axiosApiInstance.interceptors.request.use(
   async config => {
     const user = JSON.parse(localStorage.getItem("user"));
-    config.headers = { 
-      'Authorization': `Bearer ${user.token}`,
-      'Accept': 'application/json',
-      'Content-Type': 'application/x-www-form-urlencoded'
+
+    if (user) {
+      config.headers = {
+        'Authorization': `Bearer ${user.token}`,
+        'Accept': 'application/json',
+        'Content-Type': 'application/x-www-form-urlencoded'
+      }
     }
     return config;
   },
   error => {
     Promise.reject(error)
-});
+  });
 
 
 export const axiosIntercepter = axiosApiInstance;
